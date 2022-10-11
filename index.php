30f604ff63d1f2aec8cf19d9bb63eeb9ae2de991
hassy <andruhinadaria@yandex.ru>
2022-10-11 10:18:28 +0300

    html banner

--- index.php
+++ index.php
@@ -1,25 +1,31 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>  </title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title></title>
 
-  <link rel="stylesheet" href="css/bootstrap.min.css">
-  <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/index.css">
 
-  <script src="js/jQuery.js"></script>
-  <script src="js/bootstrap.min.js"></script>
-  <script src="js/jquery.maskedinput.min.js"></script>
+    <script src="js/jQuery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.maskedinput.min.js"></script>
 
-  <!--- section owlcarousel--->
-  <link rel="stylesheet" href="owlcarousel/owl.carousel.min.css">
-  <script src="owlcarousel/owl.carousel.min.js"></script>
+    <!--- section owlcarousel--->
+    <link rel="stylesheet" href="owlcarousel/owl.carousel.min.css">
+    <script src="owlcarousel/owl.carousel.min.js"></script>
 
-  <!---section fancybox--->
-  <link rel="stylesheet" href="fancybox/jquery.fancybox.min.css">
-  <script src="fancybox/jquery.fancybox.min.js"></script>
+    <!---section fancybox--->
+    <link rel="stylesheet" href="fancybox/jquery.fancybox.min.css">
+    <script src="fancybox/jquery.fancybox.min.js"></script>
+
+    <!---animated headline--->
+    <link rel="stylesheet" href="animated-headline/style.css">
+    <script src="animated-headline/main.js"></script>
+    <script src="animated-headline/modernizr.js"></script>
+
 
 </head>
 <body>
@@ -29,6 +35,72 @@
             <img src="img/logo.svg">
         </a>
     </div>
+    <div class="nav">
+        <a class="nav_item" href="#about">О компании</a>
+        <a class="nav_item" href="#whywe">Почему мы</a>
+        <a class="nav_item" href="#service">Наши услуги</a>
+        <a class="nav_item" href="#contact">Контакты</a>
+    </div>
 </header>
+<main id="up">
+    <div class="banner">
+        <div class="bg"></div>
+        <div class="wrapp container">
+            <h1 class="cd-headline letters rotate-2">
+                <span>Ремонт под ключ</span>
+                <span class="cd-words-wrapper">
+				<b class="is-visible">квартир</b>
+				<b>офисов</b>
+				<b>домов</b>
+			</span>
+            </h1>
+            <p class="sup_title">Преобразим вашу квартиру, дом или офис!</p>
+            <div class="content">
+                <div class="icons_block">
+                    <div class="col-12 block">
+                        <div class="head">
+                            <div class="icon"></div>
+                            <p class="block_title">От А до Я</p>
+                        </div>
+                        <p class="text">проведем для вас все работы, от составления сметы и выбора материалов и до
+                            уборки строительного мусора</p>
+                    </div>
+                    <div class="col-12 block">
+                        <div class="head">
+                            <div class="icon"></div>
+                            <p class="block_title">Гарантированный результат:</p>
+                        </div>
+                        <p class="text">мы работаем строго по договору, и вы получаете гарантию на все работы до 3-х
+                            лет</p>
+                    </div>
+                    <div class="col-12 block">
+                        <div class="head">
+                            <div class="icon"></div>
+                            <p class="block_title">Цены для всех</p>
+                        </div>
+                        <p class="text">вы можете позволить себе ремонт у профессионалов, ведь мы предлагаем варианты
+                            под любой бюдже!</p>
+                    </div>
+                    <div class="col-12 block">
+                        <div class="head">
+                            <div class="icon"></div>
+                            <p class="block_title">Профессиональные мастера</p>
+                        </div>
+                        <p class="text">в нашей команде все рабочие прошли обучение и обладают стажем от 2-х лет</p>
+                    </div>
+                </div>
+                <form class="form form_banner">
+                    <p class="block_title">Вызвать мастера</p>
+                    <input class="form_input" placeholder="Введите имя" name="name" required>
+                    <input class="form_input" placeholder="+7(999) 999 99 99" type="tel" required>
+                    <button class="btn">Вызвать мастера</button>
+                    <p class="text ">Нажимая на кнопку вы даёте согласие на обработку <span class="modal_open"
+                                                                                            data-modal="politic">персональных данных</span>
+                    </p>
+                </form>
+            </div>
+        </div>
+    </div>
+</main>
 </body>
 </html>
